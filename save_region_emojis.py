504c76046e1d5b80600cda68134f5742211ba3bc
Ilya29 <ilaaleksandrov08@gmail.com>
2025-02-19 00:55:48 +0300

    Edited save_region_emojis.py

--- save_region_emojis.py
+++ save_region_emojis.py
@@ -5,7 +5,7 @@ import classify_regions as cr
 df = pd.read_csv('result_tables/total_table.csv', sep=';')
 
 output_folder="region_emojis"
-
+#cccccccccc
 for region in df["Регион"]:
     filename = os.path.join(output_folder, f"{region}.svg")
     if not os.path.exists(filename):
