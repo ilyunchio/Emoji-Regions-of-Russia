83bda95c8826922f911a70b19ad0459c44274f2e
Ilya29 <ilaaleksandrov08@gmail.com>
2025-02-19 02:03:08 +0300

    Revert "Edited generate_map.py"
    This reverts commit e5f132b42ba09640103dc9f39734ef6d57e4fde0.

--- generate_map.py
+++ generate_map.py
@@ -4,4 +4,3 @@ subprocess.run(['python', 'pars_info.py'])
 subprocess.run(['python', 'clean_info.py'])
 subprocess.run(['python', 'save_region_emojis.py'])
 subprocess.run(['python', 'place_region_emojis.py'])
-#commit
