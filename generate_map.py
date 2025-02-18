e5f132b42ba09640103dc9f39734ef6d57e4fde0
Ilya29 <ilaaleksandrov08@gmail.com>
2025-02-19 00:48:49 +0300

    Edited generate_map.py

--- generate_map.py
+++ generate_map.py
@@ -4,3 +4,4 @@ subprocess.run(['python', 'pars_info.py'])
 subprocess.run(['python', 'clean_info.py'])
 subprocess.run(['python', 'save_region_emojis.py'])
 subprocess.run(['python', 'place_region_emojis.py'])
+#commit
